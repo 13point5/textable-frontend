62b289cc83712bce11e502dcfff0ce5c4f6ed4be
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-04-16 13:56:32 -0400

    update header

--- src/components/chatroom/components/header/index.tsx
+++ src/components/chatroom/components/header/index.tsx
@@ -1,16 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
-
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center p-2 bg-slate-50">
-      <Button variant="ghost" size="icon"></Button>
-
+    <div className="w-full flex justify-center items-center p-2 bg-slate-50">
       <p className="text-lg font-medium">Pierre Bot</p>
-
-      <Button variant="ghost" size="icon">
-        <MenuIcon />
-      </Button>
     </div>
   );
 };
